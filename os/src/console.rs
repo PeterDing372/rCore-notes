73903eec091e3e919e5be0c9ab09e3be35516a61
PeterDing372 <quanchending@126.com>
2024-01-23 14:23:35 -0500

    ch1 tutorial done

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,10 +1,9 @@
-//! SBI console driver, for text output
-
-use crate::sbi::console_putchar;
+// use: import a module name, crate: from current crate 
+use crate::sbi::console_putchar; 
 use core::fmt::{self, Write};
-
+/* Implementation section of (print) */
 struct Stdout;
-
+// implementation of the `Write` trait for `Stdout` struct
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
@@ -18,7 +17,6 @@ pub fn print(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
-/// print string macro
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
@@ -26,7 +24,6 @@ macro_rules! print {
     }
 }
 
-/// println string macro
 #[macro_export]
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
